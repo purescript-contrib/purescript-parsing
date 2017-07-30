499c1d0656481ba3cc30b3b09ac59941cc9312fb
Irakli Safareli <i.safareli@gmail.com>
2017-07-30 22:06:57 +0400

    add m to StreamLike

--- src/Text/Parsing/Parser/Stream.purs
+++ src/Text/Parsing/Parser/Stream.purs
@@ -2,18 +2,19 @@
 
 module Text.Parsing.Parser.Stream where
 
+import Control.Monad.State (modify, gets)
+import Control.Monad.Trans.Class (lift)
 import Data.Foldable (fold, elem, notElem)
 import Data.List as L
 import Data.List.Lazy as LazyL
-import Data.Monoid.Endo (Endo(..))
 import Data.Maybe (Maybe(..))
+import Data.Monoid.Endo (Endo(..))
 import Data.Newtype (class Newtype, unwrap)
 import Data.String as S
-import Control.Monad.State (modify, gets)
+import Prelude hiding (between)
 import Text.Parsing.Parser (ParseState(..), ParserT, fail)
 import Text.Parsing.Parser.Combinators (try, (<?>))
 import Text.Parsing.Parser.Pos (Position, updatePosString, updatePosChar)
-import Prelude hiding (between)
 
 -- | A newtype used to identify a prefix of a stream
 newtype Prefix a = Prefix a
@@ -39,41 +40,41 @@ instance charHasUpdatePosition :: HasUpdatePosition Char where
 -- |
 -- | Instances must satisfy the following laws:
 -- | - `stripPrefix (Prefix a) a >>= uncons = Nothing`
-class StreamLike f c | f -> c where
-  uncons :: f -> Maybe { head :: c, tail :: f, updatePos :: Position -> Position }
-  stripPrefix :: Prefix f -> f -> Maybe {  rest :: f, updatePos :: Position -> Position }
+class StreamLike s m t | s -> t where
+  uncons :: s -> m (Maybe { head :: t, tail :: s, updatePos :: Position -> Position })
+  stripPrefix :: Prefix s -> s -> m (Maybe {  rest :: s, updatePos :: Position -> Position })
 
-instance stringStreamLike :: StreamLike String Char where
-  uncons f = S.uncons f <#> \({ head, tail}) ->
+instance stringStreamLike :: (Applicative m) =>  StreamLike String m Char where
+  uncons f = pure $ S.uncons f <#> \({ head, tail}) ->
     { head, tail, updatePos: (_ `updatePos` head)}
-  stripPrefix (Prefix p) s = S.stripPrefix (S.Pattern p) s <#> \rest ->
+  stripPrefix (Prefix p) s = pure $ S.stripPrefix (S.Pattern p) s <#> \rest ->
     { rest, updatePos: (_ `updatePos` p)}
 
-instance listStreamLike :: (Eq a, HasUpdatePosition a) => StreamLike (L.List a) a where
-  uncons f = L.uncons f <#> \({ head, tail}) ->
+instance listStreamLike :: (Applicative m, Eq a, HasUpdatePosition a) => StreamLike (L.List a) m a where
+  uncons f = pure $ L.uncons f <#> \({ head, tail}) ->
     { head, tail, updatePos: (_ `updatePos` head)}
-  stripPrefix (Prefix p) s = L.stripPrefix (L.Pattern p) s <#> \rest ->
+  stripPrefix (Prefix p) s = pure $ L.stripPrefix (L.Pattern p) s <#> \rest ->
     { rest, updatePos: unwrap (fold (p <#> (flip updatePos >>> Endo)))}
 
-instance lazyListStreamLike :: (Eq a, HasUpdatePosition a) => StreamLike (LazyL.List a) a where
-  uncons f = LazyL.uncons f <#> \({ head, tail}) ->
+instance lazyListStreamLike :: (Applicative m, Eq a, HasUpdatePosition a) => StreamLike (LazyL.List a) m a where
+  uncons f = pure $ LazyL.uncons f <#> \({ head, tail}) ->
     { head, tail, updatePos: (_ `updatePos` head)}
-  stripPrefix (Prefix p) s = LazyL.stripPrefix (LazyL.Pattern p) s <#> \rest ->
+  stripPrefix (Prefix p) s = pure $ LazyL.stripPrefix (LazyL.Pattern p) s <#> \rest ->
     { rest, updatePos: unwrap (fold (p <#> (flip updatePos >>> Endo)))}
 
 -- | Match end of stream.
-eof :: forall f c m. StreamLike f c => Monad m => ParserT f m Unit
+eof :: forall s t m. StreamLike s m t => Monad m => ParserT s m Unit
 eof = do
   input <- gets \(ParseState input _ _) -> input
-  case uncons input of
+  (lift $ uncons input) >>= case _ of
     Nothing -> pure unit
     _ -> fail "Expected EOF"
 
 -- | Match the specified prefix.
-prefix :: forall f c m. StreamLike f c => Show f => Monad m => f -> ParserT f m f
+prefix :: forall f c m. StreamLike f m c => Show f => Monad m => f -> ParserT f m f
 prefix p = do
   input <- gets \(ParseState input _ _) -> input
-  case stripPrefix (Prefix p) input of
+  (lift $ stripPrefix (Prefix p) input) >>= case _ of
     Just {rest, updatePos} -> do
       modify \(ParseState _ position _) ->
         ParseState rest (updatePos position) true
@@ -81,10 +82,10 @@ prefix p = do
     _ -> fail ("Expected " <> show p)
 
 -- | Match any token.
-token :: forall f c m. StreamLike f c => Monad m => ParserT f m c
+token :: forall s t m. StreamLike s m t => Monad m => ParserT s m t
 token = do
   input <- gets \(ParseState input _ _) -> input
-  case uncons input of
+  (lift $ uncons input) >>= case _ of
     Nothing -> fail "Unexpected EOF"
     Just ({ head, updatePos, tail }) -> do
       modify \(ParseState _ position _) ->
@@ -92,21 +93,21 @@ token = do
       pure head
 
 -- | Match a token satisfying the specified predicate.
-satisfy :: forall f c m. StreamLike f c => Show c => Monad m => (c -> Boolean) -> ParserT f m c
+satisfy :: forall s t m. StreamLike s m t => Show t => Monad m => (t -> Boolean) -> ParserT s m t
 satisfy f = try do
   c <- token
   if f c then pure c
          else fail $ "Character " <> show c <> " did not satisfy predicate"
 
 -- | Match the specified token
-match :: forall f c m. StreamLike f c => Eq c => Show c => Monad m => c -> ParserT f m c
+match :: forall s t m. StreamLike s m t => Eq t => Show t => Monad m => t -> ParserT s m t
 match c = satisfy (_ == c) <?> show c
 
 
 -- | Match one of the tokens in the array.
-oneOf :: forall f c m. StreamLike f c => Show c => Eq c => Monad m => Array c -> ParserT f m c
+oneOf :: forall s t m. StreamLike s m t => Show t => Eq t => Monad m => Array t -> ParserT s m t
 oneOf ss = satisfy (flip elem ss) <?> ("one of " <> show ss)
 
 -- | Match any token not in the array.
-noneOf :: forall f c m. StreamLike f c => Show c => Eq c => Monad m => Array c -> ParserT f m c
+noneOf :: forall s t m. StreamLike s m t => Show t => Eq t => Monad m => Array t -> ParserT s m t
 noneOf ss = satisfy (flip notElem ss) <?> ("none of " <> show ss)
--- src/Text/Parsing/Parser/String.purs
+++ src/Text/Parsing/Parser/String.purs
@@ -10,38 +10,38 @@ import Text.Parsing.Parser.Combinators ((<?>))
 import Text.Parsing.Parser.Stream (class StreamLike, satisfy)
 
 -- | Match a whitespace characters but returns them using Array.
-whiteSpace :: forall f m. StreamLike f Char => Monad m => ParserT f m (Array Char)
+whiteSpace :: forall s m. StreamLike s m Char => Monad m => ParserT s m (Array Char)
 whiteSpace = many space
 
 -- | Skip whitespace characters.
-skipSpaces :: forall f m. StreamLike f Char => Monad m => ParserT f m Unit
+skipSpaces :: forall s m. StreamLike s m Char => Monad m => ParserT s m Unit
 skipSpaces = void whiteSpace
 
 -- | Parse a digit.  Matches any char that satisfies `Data.Char.Unicode.isDigit`.
-digit :: forall f m . StreamLike f Char => Monad m => ParserT f m Char
+digit :: forall s m . StreamLike s m Char => Monad m => ParserT s m Char
 digit = satisfy isDigit <?> "digit"
 
 -- | Parse a hex digit.  Matches any char that satisfies `Data.Char.Unicode.isHexDigit`.
-hexDigit :: forall f m . StreamLike f Char => Monad m => ParserT f m Char
+hexDigit :: forall s m . StreamLike s m Char => Monad m => ParserT s m Char
 hexDigit = satisfy isHexDigit <?> "hex digit"
 
 -- | Parse an octal digit.  Matches any char that satisfies `Data.Char.Unicode.isOctDigit`.
-octDigit :: forall f m . StreamLike f Char => Monad m => ParserT f m Char
+octDigit :: forall s m . StreamLike s m Char => Monad m => ParserT s m Char
 octDigit = satisfy isOctDigit <?> "oct digit"
 
 -- | Parse an uppercase letter.  Matches any char that satisfies `Data.Char.Unicode.isUpper`.
-upper :: forall f m . StreamLike f Char => Monad m => ParserT f m Char
+upper :: forall s m . StreamLike s m Char => Monad m => ParserT s m Char
 upper = satisfy isUpper <?> "uppercase letter"
 
 -- | Parse a space character.  Matches any char that satisfies `Data.Char.Unicode.isSpace`.
-space :: forall f m . StreamLike f Char => Monad m => ParserT f m Char
+space :: forall s m . StreamLike s m Char => Monad m => ParserT s m Char
 space = satisfy isSpace <?> "space"
 
 -- | Parse an alphabetical character.  Matches any char that satisfies `Data.Char.Unicode.isAlpha`.
-letter :: forall f m . StreamLike f Char => Monad m => ParserT f m Char
+letter :: forall s m . StreamLike s m Char => Monad m => ParserT s m Char
 letter = satisfy isAlpha <?> "letter"
 
 -- | Parse an alphabetical or numerical character.
 -- | Matches any char that satisfies `Data.Char.Unicode.isAlphaNum`.
-alphaNum :: forall f m . StreamLike f Char => Monad m => ParserT f m Char
+alphaNum :: forall s m . StreamLike s m Char => Monad m => ParserT s m Char
 alphaNum = satisfy isAlphaNum <?> "letter or digit"
